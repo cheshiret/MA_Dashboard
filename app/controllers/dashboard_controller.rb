ff62a9c1bbe185e5a43435e77d1e182cd4d2cb05
cheshiret <toa.chen@gmail.com>
2016-01-19 00:14:47 +0800

    Write first query to search the latest test result for each test script

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,13 @@
 class DashboardController < ApplicationController
   def home
-    @list=ReportDashboard.select( 'casename')#.where(id: 10)
-    @list_count=@list.count
+    @list = ReportDashboard.select( "casename")#.where(id: 10)
+    @list_count = @list.count
+    @subquery = TestResult.select("*")
+    @subquery = @subquery.order("create_date desc")
+    @testresult = TestResult.select("case_id, case_name, machine, awo_version, test_result")
+                      .from("(#{@subquery.to_sql}) as temp")
+                      .where("case_name".match("%migrauto%")).group("case_name")
+                      .order("create_date desc")
   end
 end
 
